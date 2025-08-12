f3abba7195405f30ab003849be1a253b802a78d0
elx890 <elx890@gmail.com>
2025-08-12 19:48:48 +0800

    logic 2

--- src/CalculatorLogic.java
+++ src/CalculatorLogic.java
@@ -1,5 +1,30 @@
 public class CalculatorLogic {
-}
+    private double result = 0;
+    private String lastCommand = "=";
+
+    public void calculate(double x, String command) {
+        switch (lastCommand) {
+            case "+":
+                result += x;
+                break;
+            case "-":
+                result -= x;
+                break;
+            case "*":
+                result *= x;
+                break;
+            case "/":
+                if (x == 0) {
+                    throw new ArithmeticException("不能除以0！");
+                }
+                result /= x;
+                break;
+            case "=":
+                result = x;
+                break;
+        }
+        lastCommand = command;
+    }
 
     public double getResult() {
         return result;
