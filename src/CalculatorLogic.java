9d8693aa4547c173128aecb078d5e6ce4659c8b2
elx890 <elx890@gmail.com>
2025-08-12 19:48:34 +0800

    logic 1

--- src/CalculatorLogic.java
+++ src/CalculatorLogic.java
@@ -1,2 +1,16 @@
 public class CalculatorLogic {
 }
+
+    public double getResult() {
+        return result;
+    }
+
+    public void reset() {
+        result = 0;
+        lastCommand = "=";
+    }
+
+    public String getLastCommand() {
+        return lastCommand;
+    }
+}
